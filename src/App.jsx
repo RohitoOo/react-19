3c5beb7466116f3938a262097262da64feef4331
RohitoOo <bhambhani.ba@gmail.com>
2024-11-18 14:58:46 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+// const App = () => {
+//   const names = ["Rohit Bhambhani", "Sanju Bhambhani", "Lavish Bhambhani"];
+//   const loggedIn = false
+//   const pStyle = {color : 'Blue', fontSize: '46px'}
+//   return (
+//     <>
+//       <div className="text-5xl">
+//         <p style={pStyle}>Hello</p>
+//         <ul>
+//           {names.map((name, index) => 
+//            ( <li key={index}>{name}</li>)
+//           )}
+//         </ul>
+//         {loggedIn && <h1>Hello Member</h1>}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
