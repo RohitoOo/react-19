857f1c31023692bc13ea282d70ca450709d75c3b
RohitoOo <bhambhani.ba@gmail.com>
2024-11-18 19:31:41 -0500

    Added Jobs Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
-import MainLayout from './layouts/MainLayout';
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements( <Route path="/" element={<MainLayout />}>
-    <Route index element={<HomePage/>} />
-  </Route>
-  ));
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" index element={<JobsPage />} />
+    </Route>
+  )
+);
 
-  const App = () => {
-  return (
-    
-  <RouterProvider  router={router}/>
-  );
+const App = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default App;
