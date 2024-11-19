8c87e93ee2b02fbf215f47c1ceed461cef56026d
RohitoOo <bhambhani.ba@gmail.com>
2024-11-19 13:16:51 -0500

    Added Job Page With API Call

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
+import JobPage from "./pages/JobPage";
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" index element={<JobsPage />} />
-      <Route path="/add-job" index element={<JobsPage />} />
+      <Route path="/jobs/:id" index element={<JobPage />} />
+      <Route path="/add-job" index element={<JobPage />} />
       <Route path="*" index element={<NotFound />} />
     </Route>
   )
