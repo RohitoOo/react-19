ee92868e9601100a4e83cd14aa6728db4e877995
RohitoOo <bhambhani.ba@gmail.com>
2024-11-19 12:55:42 -0500

    Added Proxy for API

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import JobListing from "./JobListing";
 import Spinner from "./Spinner";
 
-
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
+      const apiUrl = isHome
+        ? "/api/jobs?_limit=3"
+        : "/api/jobs";
+      
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
@@ -30,11 +31,8 @@ const JobListings = ({ isHome = false }) => {
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
-          {loading ? <Spinner loading={loading}/> : 
-            ""
-            }
+          {loading ? <Spinner loading={loading} /> : ""}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            
             {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
