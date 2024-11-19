9e08259d11ab2adfe7835c79fa9bb03db26603c6
RohitoOo <bhambhani.ba@gmail.com>
2024-11-19 14:51:25 -0500

    Added Alternate Data Loader

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
-import JobPage from "./pages/JobPage";
+import JobPage, {jobLoader} from "./pages/JobPage";
 
 
 
@@ -17,7 +17,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" index element={<JobsPage />} />
-      <Route path="/jobs/:id" index element={<JobPage />} />
+      <Route path="/jobs/:id" index element={<JobPage />}  loader={jobLoader} />
       <Route path="/add-job" index element={<JobPage />} />
       <Route path="*" index element={<NotFound />} />
     </Route>
